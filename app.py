0957d2ecc71ea83a3f796fb707220588063289a7
Xinjie Shen <frinkleko@gmail.com>
2025-05-06 23:20:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 哈基米打码器")
 st.write(
-    "Free Deploy顶不住了！请使用[Huggingface 上的Demo](https://huggingface.co/spaces/Frinkleko/auto-hajimi-mosaic) 或者[备用Demo链接](https://1b2c-129-170-197-100.ngrok-free.app/)."
+    "Free Deploy顶不住了！请使用[Huggingface 上的Demo](https://huggingface.co/spaces/Frinkleko/auto-hajimi-mosaic) 或者[备用Demo链接](https://lionfish-ace-freely.ngrok-free.app)."
 )
